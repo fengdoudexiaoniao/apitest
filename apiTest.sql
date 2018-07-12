b21523d9451df2029af8a13e9ee5dafb4e58ca86
fengdoudexiaoniao <1830790575@qq.com>
2018-07-12 09:46:07 +0800

    测试数据来源于数据库

---
+++ apiTest.sql
@@ -0,0 +1,47 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.6.0-m4 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `api_entity` (
+	`api_id` double ,
+	`api_desc` varchar (765),
+	`api_method` varchar (765),
+	`api_param` varchar (765),
+	`api_run` varchar (765),
+	`api_save` varchar (765),
+	`api_url` varchar (765),
+	`api_verify` varchar (765)
+); 
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('143','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('144','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('145','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('146','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('147','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('148','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('149','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('150','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('151','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('152','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('153','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('154','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('155','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('156','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('157','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('158','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('159','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('160','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('161','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('162','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('163','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('164','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('165','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('166','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('167','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('168','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('169','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('170','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('171','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
+insert into `api_entity` (`api_id`, `api_desc`, `api_method`, `api_param`, `api_run`, `api_save`, `api_url`, `api_verify`) values('172','根据用户ID获取用户信息','get','{\"uid\":\"5960817208\",\"access_token\":\"2.00Cry5VGcKfpZE45e47c9533MRNbdB\"}','y',NULL,'/2/users/show.json',NULL);
